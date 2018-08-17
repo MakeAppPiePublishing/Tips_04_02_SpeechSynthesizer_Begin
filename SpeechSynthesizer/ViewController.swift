82ab622e44c8df36f465b69423ca49a3addd67cc
Steve Lipton <steve@makeapppie.com>
2018-08-17 06:48:32 -0500

    Initial Commit

---
+++ SpeechSynthesizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechSynthesizer
+//
+//  Created by Steven Lipton on 8/17/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
